52dee2f336f833eab5bb09b0b7c11e7160dfaf53
bukmar927 <55237564+bukmar927@users.noreply.github.com>
2021-05-01 01:30:27 +0200

    2

---
+++ config.inc.php
@@ -0,0 +1,30 @@
+<?php
+$ablakcim = array(
+    'cim' => 'NOHAB-GM Alapítvány',
+);
+
+$fejlec = array(
+    'kepforras' => 'user.png',
+    'kepalt' => 'user',
+	'cim' => '',
+	'motto' => ''
+);
+
+$lablec = array(
+    'copyright' => 'Copyright '.date("Y").'.',
+    'ceg' => 'NOHAB-GM Alapítvány - Az alapítványról'
+);
+
+$oldalak = array(
+	'/' => array('fajl' => 'magunkrol', 'szoveg' => 'Magunkról', 'menun' => array(1,1)),
+	'hatter' => array('fajl' => 'hatter', 'szoveg' => 'Háttér', 'menun' => array(1,1)),
+	'kep' => array('fajl' => 'kep', 'szoveg' => 'Galéria', 'menun' => array(1,1)),
+     'feltolt' => array('fajl' => 'feltolt', 'szoveg' => '', 'menun' => array(0,0)),
+	
+);
+
+
+
+
+$hiba_oldal = array ('fajl' => '404', 'szoveg' => 'A keresett oldal nem található!');
+?>
---
+++ hatter.tpl.php
@@ -0,0 +1,109 @@
+
+
+<table border=0 cellspacing=0 cellpadding=12 width="100%" height="100%">
+<tr>
+<td valign="top">
+
+<table width="100%" border=0 cellspacing=0 cellpadding=0>
+
+<tr><td valign="top" align="right" bgcolor="#bc013c">
+<img src="images/nose0.gif" height=3 width=24 border=0 hspace=0 vspace=0 align="right"></td>
+<td valign="top" align="right" bgcolor="#bc013c" width="100%" colspan=2>
+<img src="images/space.gif" height=3 width=1 hspace=0 vspace=0 border=0 align="left"></td></tr>
+<tr><td valign="top" align="right" bgcolor="#bc013c">
+<img src="images/nose1.gif" height=4 width=24 border=0 hspace=0 vspace=0 align="right">
+</td><td valign="top" align="right" bgcolor="#fffad8" width="100%" colspan=2>
+<img src="images/space.gif" height=4 width=1 hspace=0 vspace=0 border=0 align="left"></td></tr>
+<tr><td valign="top" align="right" bgcolor="#bc013c">
+<img src="images/nose2.gif" height=17 width=24 border=0 hspace=0 vspace=0 align="right">
+</td><td valign="top" align="left" bgcolor="#bc013c" width="100%" colspan=2>
+<font face="Arial CE, Arial, Helvetica, Switzerland" color="#fffad8" size=4>
+Műszaki és történelmi háttér
+
+
+
+</font>
+</td></tr>
+<tr><td valign="top" align="right" bgcolor="#bc013c">
+<img src="images/space.gif" height=3 width=24 border=0 hspace=0 vspace=0 align="right"></td>
+<td valign="top" align="right" bgcolor="#bc013c" width="100%" colspan=2>
+<img src="images/space.gif" height=3 width=1 hspace=0 vspace=0 border=0 align="left"></td></tr>
+<tr><td valign="top" align="right">
+<img src="images/space.gif" height=3 width=24 border=0 hspace=0 vspace=0 align="right"></td>
+<td valign="top" align="right" width="100%" colspan=2>
+<img src="images/space.gif" height=3 width=1 hspace=0 vspace=0 border=0 align="left"></td></tr>
+
+<tr><td valign="top" align="right" background="images/st_gray.gif">
+<img src="images/space.gif" height=3 width=24 border=0 hspace=0 vspace=0 align="right"></td>
+<td valign="top" width="100%" background="images/st_gray.gif">
+<div align="justify">
+<font face="Arial CE, Arial, Switzerland, Helvetica" size=3>
+&nbsp;<br>
+
+Ezeken az oldalakon rövid áttekintést kívánunk adni azokról
+a mozdonyokról, melyekkel alapítványunk tevékenysége 
+a legszorosabban összefügg.<br><br>
+<ul>
+<li>Egy <a href=hu05_1.html>nemzetközi történeti áttekintésben</a>
+megismerhetjük a magyarországi NOHAB-ok őseit: az ausztrál Clyde 
+Engineering mozdonyait és az eredeti F-sorozatot, melyet az EMD 
+épített az Egyesült Államokban. A Clyde-on és a NOHAB-on kívül 
+további gyártókról is olvashatunk (AFB, BruNivel, Henschel), 
+melyek szintén epítettek hasonló gépeket az EMD licencével.<br><br>
+
+<li>Az <a href=hu05_2.html>M61-es sorozat rövid története</a>
+elmondja, hogyan kerülhetett egy svéd mozdony egy szocialista ország 
+fővonalaira, hogyan vívott ki magának példa nélküli tiszteletet és 
+rajongást, és hogyan végeztek három és fél évtized után a sorozattal.<br><br>
+
+<li>A <a href=hu05_3.html>műszaki leírásból</a> megtudhatjuk, mit
+rejt magában egy M61-es és miért különb a hazai vontatójárművek
+többségénél.<br><br>
+
+<li>A <a href=hu05_5.html>NOHAB gyár rövid története</a>
+bemutatja az M61-esek egykori gyártójának fejlődését.
+<br><br>
+
+<li>Végül egy <a href=hu05_4.html>képtár</a> a NOHAB-okról és 
+világszerte működő rokonaikról.<br><br>
+
+</ul>
+
+
+
+<br>&nbsp;
+
+</font>
+
+</div>
+
+
+
+
+</td><td valign="top" align="right" background="images/st_gray.gif">
+<img src="images/space.gif" height=3 width=24 border=0 hspace=0 vspace=0 align="right">
+</td></tr>
+
+</table>
+
+
+</td>
+</tr>
+
+
+
+
+<tr><td colspan=11 valign="middle"><center>
+<a href="../de/de_005.html"><img src="images/de24.gif" width=24 height=16 hspace=2 vspace=4 border=0 alt="De"></a>
+<a href="../en/en_005.html"><img src="images/en24.gif" width=24 height=16 hspace=2 vspace=4 border=0 alt="En"></a>
+</center></td></tr>
+
+</table>
+
+</td>
+</tr>
+
+
+</table>
+</body>
+</html>
