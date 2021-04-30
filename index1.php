d6b71db608dbc75f07dd8a0c0137329a54c96f69
bukmar927 <55237564+bukmar927@users.noreply.github.com>
2021-05-01 01:26:37 +0200

    1.verzio

---
+++ index1.php
@@ -0,0 +1,245 @@
+
+<html>
+<head>
+<STYLE TYPE="text/css" TITLE="not underlined links">
+A{text-decoration: none}
+</STYLE>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-2">
+<meta name="Author" CONTENT="gxy gfx">
+<meta name="KeyWords" CONTENT="NOHAB-GM Alapítvány, NOHAB-GM Foundation, M61, MÁV, NSB, DSB, Di3, MX, MY, V170, AFB. General Motors, Trolhattan, Sweden, Norway, Magyarország, Tapolca, M61-010, Renfe 333, CFL 1604, 202, 51, 52, 53, 54, 5100, 5200, 5300, 5400">
+<title>
+NOHAB-GM Alapítvány - Az alapítványról
+</title>
+<script language="JavaScript1.1">
+<!--
+function lu(c)
+ {
+ if(parent.location != self.location) parent.lu(c);
+ }
+//-->
+</script>
+</head>
+<body background="../bnohab.jpg"
+      text="#000000"
+      link="#bc013c"
+      alink="#d6d6d6"
+      vlink="#bc013c"
+      bgcolor="#fffad8"
+      onLoad="lu(2);">
+
+
+
+<table border=0 cellspacing=0 cellpadding=12 width="100%" height="100%">
+
+
+<tr>
+<td valign="top">
+
+<table width="100%" border=0 cellspacing=0 cellpadding=0>
+<script language="JavaScript1.1" src="hu_nfh.js"></script>
+<tr><td valign="top" align="right" bgcolor="#bc013c">
+<img src="../nose0.gif" height=3 width=24 border=0 hspace=0 vspace=0 align="right"></td>
+<td valign="top" align="right" bgcolor="#bc013c" width="100%" colspan=2>
+<img src="../space.gif" height=3 width=1 hspace=0 vspace=0 border=0 align="left"></td></tr>
+<tr><td valign="top" align="right" bgcolor="#bc013c">
+<img src="../nose1.gif" height=4 width=24 border=0 hspace=0 vspace=0 align="right">
+</td><td valign="top" align="right" bgcolor="#fffad8" width="100%" colspan=2>
+<img src="../space.gif" height=4 width=1 hspace=0 vspace=0 border=0 align="left"></td></tr>
+<tr><td valign="top" align="right" bgcolor="#bc013c">
+<img src="../nose2.gif" height=17 width=24 border=0 hspace=0 vspace=0 align="right">
+</td><td valign="top" align="left" bgcolor="#bc013c" width="100%" colspan=2>
+<font face="Arial CE, Arial, Helvetica, Switzerland" color="#fffad8" size=4>
+Az alapítványról
+
+
+
+</font>
+</td></tr>
+<tr><td valign="top" align="right" bgcolor="#bc013c">
+<img src="../space.gif" height=3 width=24 border=0 hspace=0 vspace=0 align="right"></td>
+<td valign="top" align="right" bgcolor="#bc013c" width="100%" colspan=2>
+<img src="../space.gif" height=3 width=1 hspace=0 vspace=0 border=0 align="left"></td></tr>
+<tr><td valign="top" align="right">
+<img src="../space.gif" height=3 width=24 border=0 hspace=0 vspace=0 align="right"></td>
+<td valign="top" align="right" width="100%" colspan=2>
+<img src="../space.gif" height=3 width=1 hspace=0 vspace=0 border=0 align="left"></td></tr>
+
+<tr><td valign="top" align="right" background="../st_gray.gif">
+<img src="../space.gif" height=3 width=24 border=0 hspace=0 vspace=0 align="right"></td>
+<td valign="top" width="100%" background="../st_gray.gif">
+<div align="justify">
+<font face="Arial CE, Arial, Switzerland, Helvetica" size=3>
+&nbsp;<br>
+
+<img src="../pix/010ice.jpg" width=256 height=328 hspace=12 vspace=6 border=0 align="left">
+Népes külföldi rokonságukhoz hasonlóan hazánkban is igen nagy népszerűségnek
+örvendenek az M61-es sorozatú mozdonyok, melyeket legtöbben &#150; gyártójuk nyomán
+&#150; NOHAB néven ismernek. A gépek kimagasló megbízhatóságukkal és egyszerű
+kezelhetőségükkel igen nagy megbecsülést vívtak ki maguknak a vasutasok körében
+és az utazóközönség is megkedvelte őket barátságos megjelenésük és dallamos, lágy 
+hangjuk miatt. Az M61-esek közel négy évtizedes pályafutásuk alatt valóságos legendává
+váltak a magyar vasúton és sokak szíve fájt értük, amikor 2000 karácsonya előtt
+végleg elbúcsúztak a menetrendi forgalomtól.
+<br>
+<br>
+1999-ben a NOHAB-ok néhány elkötelezett barátja elhatározta, hogy egy civil szervezet,
+a NOHAB-GM Alapítvány keretei közt tartja életben az M61-esek legendáját. Legfőbb célunk, hogy
+helyreállítsuk és működőképes állapotban őrizzük meg a sorozat egyik példányát az utókor 
+számára. Különvonatainkat non-profit alapon kívánjuk közlekedtetni, hogy a magyar
+vasútbarátok hazai viszonyok közt is méltányos áron, a mindennapos üzemnek megfelelő
+állapotában ismerhessék meg a mozdonyt. Terveink közt szerepel népszerűsítő előadások,
+kiállítások szervezése és ismeretterjesztő kiadványok megjelentetése is, melyeknek a laikus 
+érdeklődők és a hozzáértők egyaránt hasznát vehetik. Tevékenységünk ugyan úttörő jellegűnek
+számít hazánkban, de eddigi eredményeink mellett az is bíztató, hogy Európában és világszerte
+számos hasonló szervezet munkáját koronázta már siker.
+<br>
+<br>
+
+Alapítványunk az <b>M61-010</b> pályaszámú mozdony megőrzését tűzte ki célul,
+mivel ezt a gépet egyik alapítónk már több mint tizenöt éve segíti jelentős anyagi
+áldozattal és kétkezi munkával a túlélés felé vezető úton. A tapolcai honállomás 
+közbenjárása mellett ennek köszönhető, hogy a mozdony már kétszer megmenekült a lángvágótól,
+sőt, 1997-ben sor kerülhetett magánkézből finanszírozott külső és részleges belső 
+felújítására is.
+<br>
+<br>
+
+A mozdony megvásárlását illetően alapítványunk tagjai már 1998. óta kapcsolatban
+álltak a MÁV-val. Az évek során ugyan számos akadályt kellett leküzdenünk, ám
+erőfeszítéseinket végül siker koronázta: 2001 novemberében megszületett a mozdony 
+megvásárlásáról szóló szerződés. Mivel a mozdony eladása egy versenytárgyalás keretei
+közt történt, így alapítványunknak az eredeti várakozásokhoz képest igen magas
+árat, 8&nbsp;125&nbsp;000&nbsp;Ft-ot kell fizetnie a mozdonyért. Ez többszörösen
+meghaladja azt az összeget, amennyiért hozzánk hasonló nyugat-európai vasútbarát
+társaságok szerezték be megőrizni kívánt járműveiket.
+<br>
+<br>
+
+Alapítványunk immár kifizette a vételárat és a mozdony időközben új telephelyére, a Keleti
+pályaudvar egykori fűtőházába került. További jelentős próbatételeknek nézünk azonban elébe;
+hátra van ugyanis a mozdony üzemképessé tétele, restaurálása, melyhez további támogatókat,
+szponzorokat keresünk. A mozdony már a felújítási munkák során ingyenesen megtekinthető,
+ehhez csupán a látogatás időpontjának előzetes egyeztetése szükséges. A későbbiekben
+lehetőséget kívánunk nyújtani arra is, hogy az érdeklődők szaktudásuknak megfelelően
+maguk is kivegyék részüket a helyreállítási munkákból. A helyreállítás tervezett menetéről, az eddig elvégzett 
+műveletekről <a href="hu00_1.html">itt</a> található bővebb információ.
+<br>
+<br>
+
+Alapítványunkat 2000 januárjában kiemelten közhasznú társadalmi szervezetként jegyezte
+be a bíróság. Az adományozó cégek &#150; a megfelelő feltételek mellett &#150; az
+alapítvány számára juttatott adományok 150%-át írhatják le az adóalapból, magánszemélyek
+pedig személyi <a href="hu_001.html#1p">jövedelemadójuk 1%-át</a> is felajánlhatják.
+Alapítványunk elsősorban a magánszemélyek, érdeklődő vasútbarátok számára egy többlépcsős
+<a href="hu_001.html">tagsági rendszert</a> dolgozott ki, melynek &#150; különösen hazai
+érdeklődőkre gondolva &#150; egy kedvezményes &#132;diák&#148; kategória is része. Bármilyen 
+egyéb támogatást is szívesen fogadunk, és a segítséget igyekezzük méltó módon viszonozni. A
+tagsági díjat fizető magánszemélyek &#150; az ingyenes ajándékokon túl &#150; jelentős
+kedvezményeket kapnak különvonatainkon, a támogató cégek pedig megegyezés alapján lehetőséget
+kapnak kiadványainkban, rendezvényeinken történő bemutatkozásra. 
+<br>
+<br>
+
+Tevékenységünkről készséggel adunk <a href="hu_002.html">további tájékoztatást</a>. 
+Előre is köszönjük az érdeklődést és támogatást!
+<br>
+<br>
+<br>
+A NOHAB-GM Alapítvány
+<br>
+<br>
+<center>
+<hr width="30%" noshade>
+</center>
+<br>
+<br>
+<ul>
+<li>A NOHAB-GM Alapítvány <a href="ngmalapito.pdf">alapító okirata</a></li>
+<li>Közhasznúsági jelentés a <a href="ngmkozh00.pdf">2000-es évre</a></li>
+<li>Közhasznúsági jelentés a <a href="ngmkozh01.pdf">2001-es évre</a></li>
+<li>Közhasznúsági jelentés a <a href="ngmkozh02.pdf">2002-es évre</a></li>
+<li>Közhasznúsági jelentés a <a href="ngmkozh03.pdf">2003-as évre</a></li>
+<li>Közhasznúsági jelentés a <a href="ngmkozh04.pdf">2004-es évre</a></li>
+<li>Közhasznúsági jelentés a <a href="ngmkozh05.pdf">2005-ös évre</a></li>
+<li>Közhasznúsági jelentés a <a href="ngmkozh06.pdf">2006-os évre</a></li>
+<li>Közhasznúsági jelentés a <a href="ngmkozh07.pdf">2007-es évre</a></li>
+<li>Közhasznúsági jelentés a <a href="ngmkozh08.pdf">2008-as évre</a></li>
+<li>Közhasznúsági jelentés a <a href="ngmkozh09.pdf">2009-es évre</a></li>
+<li>Közhasznúsági jelentés a <a href="ngmkozh10.pdf">2010-es évre</a></li>
+<li>Közhasznúsági jelentés a <a href="ngmkozh11.pdf">2011-es évre</a> és pénzügyi beszámoló a <a href="ngmpb11.pdf">2011-es évre</a></li>
+<li>Pénzügyi beszámoló és közhasznúsági melléklet a <a href="ngmkozh12.pdf">2012-es évre</a></li>
+<li>Pénzügyi beszámoló és közhasznúsági melléklet a <a href="ngmkozh13.pdf">2013-as évre</a></li>
+<li>Pénzügyi beszámoló és közhasznúsági melléklet a <a href="ngmkozh14.pdf">2014-es évre</a></li>
+<li>Pénzügyi beszámoló és közhasznúsági melléklet a <a href="ngmkozh15.pdf">2015-ös évre</a></li>
+<li>Pénzügyi beszámoló és közhasznúsági melléklet a <a href="ngmkozh16.pdf">2016-os évre</a></li>
+<li>Pénzügyi beszámoló és közhasznúsági melléklet a <a href="ngmkozh17.pdf">2017-es évre</a></li>
+<li>Pénzügyi beszámoló és közhasznúsági melléklet a <a href="ngmkozh18.pdf">2018-as évre</a></li>
+</ul>
+<br>&nbsp;
+
+</font>
+
+</div>
+
+
+
+
+</td><td valign="top" align="right" background="../st_gray.gif">
+<img src="../space.gif" height=3 width=24 border=0 hspace=0 vspace=0 align="right">
+</td></tr>
+
+</table>
+
+
+</td>
+</tr>
+
+
+<tr>
+<td valign="bottom">
+<table width="100%" border=0 cellspacing=0 cellpadding=0>
+
+<tr><td colspan=11 bgcolor="#bc013c" ><img src="../space.gif" border=0 hspace=0 vspace=0 height=1 width=1 align="left"></td></tr>
+<tr><td colspan=11 ><img src="../space.gif" border=0 hspace=0 vspace=0 height=4 width=1 align="left"></td></tr>
+
+
+<tr>
+<td valign="top" width="15%"><center><a href="hu_000.html"><font face="Arial CE, Arial, Helvetica, Switzerland" color="#bc013c" size=2>
+Magunkról
+</font></a></center></td><td valign="top" width="1%"><center><b><font face="Arial CE, Arial, Helvetica, Switzerland" color="#bc013c" size=2>&middot;</font></b></center></td>
+<td valign="top" width="15%"><center><a href="hu_005.html"><font face="Arial CE, Arial, Helvetica, Switzerland" color="#bc013c" size=2>
+Háttér
+</font></a></center></td><td valign="top" width="1%"><center><b><font face="Arial CE, Arial, Helvetica, Switzerland" color="#bc013c" size=2>&middot;</font></b></center></td>
+<td valign="top" width="15%"><center><a href="hu_001.html"><font face="Arial CE, Arial, Helvetica, Switzerland" color="#bc013c" size=2>
+Tagság
+</font></a></center></td><td valign="top" width="1%"><center><b><font face="Arial CE, Arial, Helvetica, Switzerland" color="#bc013c" size=2>&middot;</font></b></center></td>
+<td valign="top" width="15%"><center><a href="hu_002.html"><font face="Arial CE, Arial, Helvetica, Switzerland" color="#bc013c" size=2>
+Elérhetőség
+</font></a></center></td><td valign="top" width="1%"><center><b><font face="Arial CE, Arial, Helvetica, Switzerland" color="#bc013c" size=2>&middot;</font></b></center></td>
+<td valign="top" width="15%"><center><a href="hu_003.html"><font face="Arial CE, Arial, Helvetica, Switzerland" color="#bc013c" size=2>
+Aktuális
+</font></a></center></td><td valign="top" width="1%"><center><b><font face="Arial CE, Arial, Helvetica, Switzerland" color="#bc013c" size=2>&middot;</font></b></center></td>
+<td valign="top" width="15%"><center><a href="hu_004.html"><font face="Arial CE, Arial, Helvetica, Switzerland" color="#bc013c" size=2>
+Linkek
+</font></a></center></td>
+</tr>
+
+<tr><td colspan=11 ><img src="../space.gif" border=0 hspace=0 vspace=0 height=4 width=1 align="left"></td></tr>
+
+<tr><td colspan=11 bgcolor="#bc013c" ><img src="../space.gif" border=0 hspace=0 vspace=0 height=1 width=1 align="left"></td></tr>
+<tr><td colspan=11 ><img src="../space.gif" border=0 hspace=0 vspace=0 height=4 width=1 align="left"></td></tr>
+
+<tr><td colspan=11 valign="middle"><center>
+<a href="../de/de_000.html"><img src="../de24.gif" width=24 height=16 hspace=2 vspace=4 border=0 alt="De"></a>
+<a href="../en/en_000.html"><img src="../en24.gif" width=24 height=16 hspace=2 vspace=4 border=0 alt="En"></a>
+</center></td></tr>
+
+</table>
+
+</td>
+</tr>
+
+
+</table>
+</body>
+</html>
---
+++ rolunk.php
@@ -0,0 +1,172 @@
+
+<html>
+<head>
+<STYLE TYPE="text/css" TITLE="not underlined links">
+A{text-decoration: none}
+</STYLE>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-2">
+<meta name="Author" CONTENT="gxy gfx">
+<meta name="KeyWords" CONTENT="NOHAB-GM Alapítvány, NOHAB-GM Foundation, M61, MÁV, NSB, DSB, Di3, MX, MY, V170, AFB. General Motors, Trolhattan, Sweden, Norway, Magyarország, Tapolca, M61-010, Renfe 333, CFL 1604, 202, 51, 52, 53, 54, 5100, 5200, 5300, 5400">
+<title>
+NOHAB-GM Alapítvány - Műszaki és történelmi háttér
+</title>
+<script language="JavaScript1.1">
+<!--
+function lu(c)
+ {
+ if(parent.location != self.location) parent.lu(c);
+ }
+//-->
+</script>
+</head>
+<body background="../bnohab.jpg"
+      text="#000000"
+      link="#bc013c"
+      alink="#d6d6d6"
+      vlink="#bc013c"
+      bgcolor="#fffad8"
+      onLoad="lu(2);">
+
+
+
+<table border=0 cellspacing=0 cellpadding=12 width="100%" height="100%">
+
+
+<tr>
+<td valign="top">
+
+<table width="100%" border=0 cellspacing=0 cellpadding=0>
+<script language="JavaScript1.1" src="hu_nfh.js"></script>
+<tr><td valign="top" align="right" bgcolor="#bc013c">
+<img src="../nose0.gif" height=3 width=24 border=0 hspace=0 vspace=0 align="right"></td>
+<td valign="top" align="right" bgcolor="#bc013c" width="100%" colspan=2>
+<img src="../space.gif" height=3 width=1 hspace=0 vspace=0 border=0 align="left"></td></tr>
+<tr><td valign="top" align="right" bgcolor="#bc013c">
+<img src="../nose1.gif" height=4 width=24 border=0 hspace=0 vspace=0 align="right">
+</td><td valign="top" align="right" bgcolor="#fffad8" width="100%" colspan=2>
+<img src="../space.gif" height=4 width=1 hspace=0 vspace=0 border=0 align="left"></td></tr>
+<tr><td valign="top" align="right" bgcolor="#bc013c">
+<img src="../nose2.gif" height=17 width=24 border=0 hspace=0 vspace=0 align="right">
+</td><td valign="top" align="left" bgcolor="#bc013c" width="100%" colspan=2>
+<font face="Arial CE, Arial, Helvetica, Switzerland" color="#fffad8" size=4>
+Műszaki és történelmi háttér
+
+
+
+</font>
+</td></tr>
+<tr><td valign="top" align="right" bgcolor="#bc013c">
+<img src="../space.gif" height=3 width=24 border=0 hspace=0 vspace=0 align="right"></td>
+<td valign="top" align="right" bgcolor="#bc013c" width="100%" colspan=2>
+<img src="../space.gif" height=3 width=1 hspace=0 vspace=0 border=0 align="left"></td></tr>
+<tr><td valign="top" align="right">
+<img src="../space.gif" height=3 width=24 border=0 hspace=0 vspace=0 align="right"></td>
+<td valign="top" align="right" width="100%" colspan=2>
+<img src="../space.gif" height=3 width=1 hspace=0 vspace=0 border=0 align="left"></td></tr>
+
+<tr><td valign="top" align="right" background="../st_gray.gif">
+<img src="../space.gif" height=3 width=24 border=0 hspace=0 vspace=0 align="right"></td>
+<td valign="top" width="100%" background="../st_gray.gif">
+<div align="justify">
+<font face="Arial CE, Arial, Switzerland, Helvetica" size=3>
+&nbsp;<br>
+
+Ezeken az oldalakon rövid áttekintést kívánunk adni azokról
+a mozdonyokról, melyekkel alapítványunk tevékenysége 
+a legszorosabban összefügg.<br><br>
+<ul>
+<li>Egy <a href=hu05_1.html>nemzetközi történeti áttekintésben</a>
+megismerhetjük a magyarországi NOHAB-ok őseit: az ausztrál Clyde 
+Engineering mozdonyait és az eredeti F-sorozatot, melyet az EMD 
+épített az Egyesült Államokban. A Clyde-on és a NOHAB-on kívül 
+további gyártókról is olvashatunk (AFB, BruNivel, Henschel), 
+melyek szintén epítettek hasonló gépeket az EMD licencével.<br><br>
+
+<li>Az <a href=hu05_2.html>M61-es sorozat rövid története</a>
+elmondja, hogyan kerülhetett egy svéd mozdony egy szocialista ország 
+fővonalaira, hogyan vívott ki magának példa nélküli tiszteletet és 
+rajongást, és hogyan végeztek három és fél évtized után a sorozattal.<br><br>
+
+<li>A <a href=hu05_3.html>műszaki leírásból</a> megtudhatjuk, mit
+rejt magában egy M61-es és miért különb a hazai vontatójárművek
+többségénél.<br><br>
+
+<li>A <a href=hu05_5.html>NOHAB gyár rövid története</a>
+bemutatja az M61-esek egykori gyártójának fejlődését.
+<br><br>
+
+<li>Végül egy <a href=hu05_4.html>képtár</a> a NOHAB-okról és 
+világszerte működő rokonaikról.<br><br>
+
+</ul>
+
+
+
+<br>&nbsp;
+
+</font>
+
+</div>
+
+
+
+
+</td><td valign="top" align="right" background="../st_gray.gif">
+<img src="../space.gif" height=3 width=24 border=0 hspace=0 vspace=0 align="right">
+</td></tr>
+
+</table>
+
+
+</td>
+</tr>
+
+
+<tr>
+<td valign="bottom">
+<table width="100%" border=0 cellspacing=0 cellpadding=0>
+
+<tr><td colspan=11 bgcolor="#bc013c" ><img src="../space.gif" border=0 hspace=0 vspace=0 height=1 width=1 align="left"></td></tr>
+<tr><td colspan=11 ><img src="../space.gif" border=0 hspace=0 vspace=0 height=4 width=1 align="left"></td></tr>
+
+
+<tr>
+<td valign="top" width="15%"><center><a href="hu_000.html"><font face="Arial CE, Arial, Helvetica, Switzerland" color="#bc013c" size=2>
+Magunkról
+</font></a></center></td><td valign="top" width="1%"><center><b><font face="Arial CE, Arial, Helvetica, Switzerland" color="#bc013c" size=2>&middot;</font></b></center></td>
+<td valign="top" width="15%"><center><a href="hu_005.html"><font face="Arial CE, Arial, Helvetica, Switzerland" color="#bc013c" size=2>
+Háttér
+</font></a></center></td><td valign="top" width="1%"><center><b><font face="Arial CE, Arial, Helvetica, Switzerland" color="#bc013c" size=2>&middot;</font></b></center></td>
+<td valign="top" width="15%"><center><a href="hu_001.html"><font face="Arial CE, Arial, Helvetica, Switzerland" color="#bc013c" size=2>
+Tagság
+</font></a></center></td><td valign="top" width="1%"><center><b><font face="Arial CE, Arial, Helvetica, Switzerland" color="#bc013c" size=2>&middot;</font></b></center></td>
+<td valign="top" width="15%"><center><a href="hu_002.html"><font face="Arial CE, Arial, Helvetica, Switzerland" color="#bc013c" size=2>
+Elérhetőség
+</font></a></center></td><td valign="top" width="1%"><center><b><font face="Arial CE, Arial, Helvetica, Switzerland" color="#bc013c" size=2>&middot;</font></b></center></td>
+<td valign="top" width="15%"><center><a href="hu_003.html"><font face="Arial CE, Arial, Helvetica, Switzerland" color="#bc013c" size=2>
+Aktuális
+</font></a></center></td><td valign="top" width="1%"><center><b><font face="Arial CE, Arial, Helvetica, Switzerland" color="#bc013c" size=2>&middot;</font></b></center></td>
+<td valign="top" width="15%"><center><a href="hu_004.html"><font face="Arial CE, Arial, Helvetica, Switzerland" color="#bc013c" size=2>
+Linkek
+</font></a></center></td>
+</tr>
+
+<tr><td colspan=11 ><img src="../space.gif" border=0 hspace=0 vspace=0 height=4 width=1 align="left"></td></tr>
+
+<tr><td colspan=11 bgcolor="#bc013c" ><img src="../space.gif" border=0 hspace=0 vspace=0 height=1 width=1 align="left"></td></tr>
+<tr><td colspan=11 ><img src="../space.gif" border=0 hspace=0 vspace=0 height=4 width=1 align="left"></td></tr>
+
+<tr><td colspan=11 valign="middle"><center>
+<a href="../de/de_005.html"><img src="../de24.gif" width=24 height=16 hspace=2 vspace=4 border=0 alt="De"></a>
+<a href="../en/en_005.html"><img src="../en24.gif" width=24 height=16 hspace=2 vspace=4 border=0 alt="En"></a>
+</center></td></tr>
+
+</table>
+
+</td>
+</tr>
+
+
+</table>
+</body>
+</html>
